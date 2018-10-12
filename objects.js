f5daecdca08976c07e50252ff67f3f78bf27d4a7
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-12 00:18:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,15 +1,12 @@
-var playlist = {
-  artist: "Led Zeppelin",
-  song: "Ramble On"
-}
+var playlist = {}
+playlist['Led Zeppelin'] = 'Ramble On'
 
 function updatePlaylist(playlist, artist, song){
-  playlist.artist = artist
-  playlist.song = song
+  playlist[artist] = song
   return playlist
 }
 
 function removeFromPlaylist(playlist, artist){
-  delete playlist.artist
+  delete playlist[artist]
   return playlist
 }

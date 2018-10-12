fe5cb0cc48b64710612e71a35b46fbcd60deeabc
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-12 00:01:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,15 @@
 var playlist = {
-  
+  artist: "Led Zeppelin",
+  song: "Ramble On"
+}
+
+function updatePlaylist(playlist, artist, song){
+  playlist.artist = artist
+  playlist.song = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist
+  return playlist
 }
